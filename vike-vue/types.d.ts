36746e2fde896b21c505210c94bce5a7b218cfa8
Aurelien Lourot <software@lourot.dev>
2023-08-18 18:13:42 +0200

    Fix "Cannot find module './PageShell.vue'"

--- vike-vue/types.d.ts
+++ vike-vue/types.d.ts
@@ -1,5 +1,8 @@
-export type {
-  PageContextBuiltIn,
-  PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient
-} from 'vite-plugin-ssr/types'
-export type { ConfigEnhanced as Config } from './renderer/+config'
+declare module '*.vue' {
+  const Component: any
+  export default Component
+}
+declare module '*.svg' {
+  const imageUrl: string
+  export default imageUrl
+}
