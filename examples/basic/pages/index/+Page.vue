89a68fff038211e803e3b70e6a2f8ef108f515c4
Aurelien Lourot <software@lourot.dev>
2023-08-19 19:53:52 +0200

    Add Counter component

--- examples/basic/pages/index/+Page.vue
+++ examples/basic/pages/index/+Page.vue
@@ -3,8 +3,12 @@
   This page is:
   <ul>
     <li>Rendered to HTML.</li>
-    <li>Interactive.</li>
+    <li>Interactive. <Counter /></li>
   </ul>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts">
+import Counter from '../../components/Counter.vue'
+const components = { Counter }
+export default { components }
+</script>
