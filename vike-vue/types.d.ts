3cb3bf722a795b1f1ed8c10a39d07dda1c8320f8
Aurelien Lourot <software@lourot.dev>
2023-08-19 13:04:54 +0200

    Fix type import from vike-vue/types

--- vike-vue/types.d.ts
+++ vike-vue/types.d.ts
@@ -6,3 +6,9 @@ declare module '*.svg' {
   const imageUrl: string
   export default imageUrl
 }
+
+export type {
+  PageContextBuiltIn,
+  PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient
+} from 'vite-plugin-ssr/types'
+export type { ConfigEnhanced as Config } from './renderer/+config'
