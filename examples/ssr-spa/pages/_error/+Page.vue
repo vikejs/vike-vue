030ca2318510c6db321f1825ea93f87d217d4d5f
Aurelien Lourot <software@lourot.dev>
2023-08-24 11:32:21 +0200

    examples: add _error page

---
+++ examples/ssr-spa/pages/_error/+Page.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="center">
+    <p>{{ abortReason }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAttrs, computed } from 'vue'
+import { usePageContext } from 'vike-vue/usePageContext'
+
+const ctx = usePageContext()
+let { is404, abortReason } = ctx
+if (!abortReason) {
+  abortReason = is404 ? 'Page not found.' : 'Something went wrong.'
+}
+</script>
+
+<style>
+.center {
+  height: calc(100vh - 100px);
+  display: flex;
+  font-size: 1.3em;
+  justify-content: center;
+  align-items: center;
+}
+</style>
