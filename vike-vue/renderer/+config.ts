11417cac897d4405af393ff5312ae746e5ce1662
Aurelien Lourot <software@lourot.dev>
2023-09-08 17:36:29 +0200

    fix: Fix `__name is not defined`
    when running with tsx.

--- vike-vue/renderer/+config.ts
+++ vike-vue/renderer/+config.ts
@@ -94,3 +94,25 @@ export default {
     }
   }
 } satisfies ConfigCore
+
+// This is a workaround for
+// * https://github.com/vuejs/core/issues/8303
+// * https://github.com/esbuild-kit/tsx/issues/113
+// Without this, when running vike-vue with tsx (for example when scaffolding a
+// Vue+Express project with Bati), querying the server will fail with the
+// following error:
+//     [vite-plugin-ssr][request(1)] HTTP request: /
+//     [vite][request(1)] __name is not defined
+//     [vite][request(1)] __name is not defined
+//     [vite][request(1)] __name is not defined
+//     [vite][request(1)] Error when evaluating SSR module virtual:vite-plugin-ssr:importPageCode:server:/pages/index: failed to import "/pages/index/+Page.vue"
+//     |- ReferenceError: __name is not defined
+const globalName = (target: Object, value: string) =>
+  Object.defineProperty(target, 'name', {
+    value,
+    configurable: true
+  })
+declare global {
+  var __name: typeof globalName
+}
+globalThis.__name = globalName
