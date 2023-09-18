b5c47a488b3feb2684bdc95cad9a28765bcbb334
Aurelien Lourot <software@lourot.dev>
2023-09-18 17:31:04 +0200

    qual: Remove now useless type re-exports
    Same as https://github.com/vikejs/vike-react/commit/93b83833

--- vike-vue/types.d.ts
+++ vike-vue/types.d.ts
@@ -6,9 +6,3 @@ declare module '*.svg' {
   const imageUrl: string
   export default imageUrl
 }
-
-export type {
-  PageContextBuiltIn,
-  PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient
-} from 'vite-plugin-ssr/types'
-import type { Component } from './renderer/types'
