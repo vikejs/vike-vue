cc15925b2260bc3b4045b98abb04e64a67553912
Aurelien Lourot <software@lourot.dev>
2023-09-23 11:46:15 +0200

    example: use real Vue plugin instead of dummy one // #16

--- examples/with-vue-plugin/pages/+vuePlugins.ts
+++ examples/with-vue-plugin/pages/+vuePlugins.ts
@@ -1,16 +1,9 @@
-import type { Plugin } from 'vue'
-
-// TODO: use a real plugin instead of this dummy plugin for this example
-const myPlugin: Plugin = {
-  install(app, options) {
-    console.log('myPlugin.install():', options)
-  }
-}
+import ToastPlugin from 'vue-toast-notification'
 
 // List of Vue plugins to install.
 export default [
   {
-    plugin: myPlugin,
-    options: { foo: 'bar' }
+    plugin: ToastPlugin
+    // , options: { foo: 'bar' }
   }
 ]
--- examples/with-vue-plugin/pages/index/+Page.vue
+++ examples/with-vue-plugin/pages/index/+Page.vue
@@ -12,3 +12,21 @@ import Counter from '../../components/Counter.vue'
 const components = { Counter }
 export default { components }
 </script>
+
+<script lang="ts" setup>
+import { useToast } from 'vue-toast-notification'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const toastPlugin = useToast()
+  toastPlugin.success('I am a Vue plugin!', {
+    position: 'bottom',
+    duration: 0 // infinite
+  })
+})
+</script>
+
+<style>
+/* see https://stackoverflow.com/questions/55206901/how-to-import-css-files-in-vue-3-child-components */
+@import 'vue-toast-notification/dist/theme-sugar.css';
+</style>
