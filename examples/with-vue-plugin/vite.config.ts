c3a0fc217e08dbe12f974d58b1272b797321e0b3
Muhammad <phonzammizaki@gmail.com>
2023-12-18 22:37:06 +0700

    minor

--- examples/with-vue-plugin/vite.config.ts
+++ examples/with-vue-plugin/vite.config.ts
@@ -1,10 +1,10 @@
 import vue from '@vitejs/plugin-vue'
-import ssr from 'vike/plugin'
+import vike from 'vike/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [
-    ssr({ prerender: true }),
+    vike({ prerender: true }),
     vue({
       include: [/\.vue$/, /\.md$/]
     })
