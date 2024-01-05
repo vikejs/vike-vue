eaaed583f5dd3ad8143f539dff94b2e6dd7d7a71
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-01-05 02:40:00 +0100

    make async component non-suspensible, this is needed for it to show its error / loading components
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- vike-vue/components/ClientOnly.vue
+++ vike-vue/components/ClientOnly.vue
@@ -1,14 +1,15 @@
 <template>
   <Suspense>
-    <component :is="comp" v-bind="$attrs" />
-    <template #fallback>
+    <component :is="shown ? AsyncComponent : EmptyComponent" v-bind="$attrs" />
+    <template v-if="!shown" #fallback>
       <slot name="fallback" />
     </template>
   </Suspense>
 </template>
 
 <script lang="ts" setup generic="T extends AsyncComponentLoader">
-import { h, onMounted, defineComponent, defineAsyncComponent, shallowRef, type AsyncComponentLoader } from 'vue';
+import { h, ref, onMounted, defineComponent, defineAsyncComponent, useSlots, type AsyncComponentLoader } from 'vue';
+import type { Component } from '../renderer/types';
 
 defineOptions({ inheritAttrs: false })
 
@@ -18,24 +19,34 @@ type Props = {
 
 const props = defineProps<Props>()
 
+type Slots = {
+  fallback?: () => Component
+  error?: () => Component
+}
+
+defineSlots<Slots>()
+const slots = useSlots()
+
 const ClientOnlyError = defineComponent({
   setup: () => h("p", "Error loading component"),
 })
 
 const AsyncComponent = defineAsyncComponent({
   loader: props.load,
-  errorComponent: ClientOnlyError,
+  loadingComponent: slots.fallback,
+  errorComponent: slots.error ?? ClientOnlyError,
   onError: (e) => {
     console.error("Component loading failed:", e)
   },
+  suspensible: false,
 })
 
 const EmptyComponent = defineComponent({
   render: () => null,
 })
 
-const comp = shallowRef<any>(EmptyComponent)
+const shown = ref(false)
 onMounted(() => {
-  comp.value = AsyncComponent
+  shown.value = true
 })
 </script>
