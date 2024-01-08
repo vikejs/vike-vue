c972b900a69257d6647a4af19e334ebee39ec2cd
Romuald Brillout <git@brillout.com>
2024-01-08 22:15:20 +0100

    minor

--- vike-vue/vite.config.ts
+++ vike-vue/vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'node:path'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), dts()],
   build: {
