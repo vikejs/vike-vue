cf09ccb584fe1399a81d8edd4636c06ca0c111e7
Chris-Robin Ennen <chris-robin@ennen.dev>
2024-01-12 14:08:51 +0100

    make vike-pinia use `onCreateAppPinia` so users don't have to overwrite the `onCreateApp` hook
    Signed-off-by: Chris-Robin Ennen <chris-robin@ennen.dev>

--- packages/vike-pinia/renderer/hydratePinia.ts
+++ packages/vike-pinia/renderer/hydratePinia.ts
@@ -6,15 +6,10 @@ export { hydratePinia }
 type PiniaOnBeforeMountApp = OnBeforeMountApp<PiniaAdditionalSSRContext>
 
 const hydratePinia: PiniaOnBeforeMountApp = ({ pinia, fromHtmlRenderer }): ReturnType<PiniaOnBeforeMountApp> => {
-  if (!pinia) {
-    console.warn("[vike-pinia] Missing pinia from pageContext. Did you forget to use `installPinia()` in `+onCreateApp()`?")
-    return
-  }
-
   const { piniaInitialState } = fromHtmlRenderer
 
-  if (!piniaInitialState) {
-    console.warn("[vike-pinia] No initial state was found. Did you forget to return `initialStoreState` from `+onRenderHtml()`?")
+  if (!pinia || !piniaInitialState) {
+    // happens if SSR is off
     return
   }
 
