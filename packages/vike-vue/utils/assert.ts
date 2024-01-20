c8ad00a267e10055ca32f74a3cc9f2faa3901735
Romuald Brillout <git@brillout.com>
2024-01-20 14:10:03 +0100

    minor

--- packages/vike-vue/utils/assert.ts
+++ packages/vike-vue/utils/assert.ts
@@ -1,4 +1,4 @@
 export function assert(condition: unknown): asserts condition {
   if (condition) return
-  throw new Error("You stumbled upon a bug in vike-vue's source code. Reach out on GitHub and we will fix the bug.")
+  throw new Error('You stumbled upon a vike-vue bug. Reach out on GitHub and a maintainer will fix the bug.')
 }
