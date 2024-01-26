47076e6816bb872110606637ca74adf5072810ea
Aurelien Lourot <software@lourot.dev>
2024-01-26 11:51:07 +0100

    Skip canonical URLs for more error pages

--- examples/basic/layouts/HeadDefault.vue
+++ examples/basic/layouts/HeadDefault.vue
@@ -2,30 +2,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="description" content="Demo showcasing Vike + Vue" />
   <link rel="icon" :href="logoUrl" />
-  <link v-if="canonicalUrl" rel="canonical" :href="canonicalUrl">
+  <link v-if="canonicalUrl" rel="canonical" :href="canonicalUrl" />
 </template>
 
 <script lang="ts" setup>
 import logoUrl from '../assets/logo.svg'
 
 import { usePageContext } from 'vike-vue/usePageContext'
-import { PageContext } from 'vike/types';
+import { PageContext } from 'vike/types'
 
-const pageContext = usePageContext();
+const pageContext = usePageContext()
 
 function getCanonicalUrl(pageContext: PageContext): null | string {
-
-  // A canonical URL for a 404 page would be nonsensical in the general case.
+  // In general a canonical URL on an error page doesn't make much sense.
+  // On the other hand, in the future we might want to support setting a canonical URL on a 404
+  // page for handling link deprecation.
   //
-  // If, on the other hand, you deprecate links to old content, for SEO you may 
-  // need to decide whether to return a 404 (optionally with a canonical URL),
-  // or a 301, or to handle the link deprecation in some other fashion. Good luck.
-  if (pageContext.is404) {
+  // See also https://vike.dev/render
+  if (pageContext.is404 || pageContext.abortStatusCode || pageContext.abortReason) {
     return null
   }
-    
-  return new URL(pageContext.urlPathname, pageContext.config.baseUrl).toString();
+  return new URL(pageContext.urlPathname, pageContext.config.baseUrl).toString()
 }
 
-const canonicalUrl = getCanonicalUrl(pageContext);
+const canonicalUrl = getCanonicalUrl(pageContext)
 </script>
