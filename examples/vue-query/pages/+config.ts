1d091d9d3e8aaa0feca8405a43f396e3a5df5db7
Dan Popescu <pdan.popescu@gmail.com>
2024-06-03 13:48:52 +0300

    fix: export Vike configuration at /config for vike-vue-query BREAKING CHANGE: use `import vikeVueQuery from 'vike-vue-query/config'` in `+config.`

--- examples/vue-query/pages/+config.ts
+++ examples/vue-query/pages/+config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'vike/types'
 import Layout from '../layouts/LayoutDefault.vue'
 import Head from '../layouts/HeadDefault.vue'
 import vikeVue from 'vike-vue/config'
-import vikeVueQuery from 'vike-vue-query'
+import vikeVueQuery from 'vike-vue-query/config'
 
 // Default configs (can be overridden by pages)
 export default {
