dcec9c02ffb9dd6422e7efb47b08fe408f30397e
Romuald Brillout <git@brillout.com>
2024-06-13 14:42:01 +0200

    polish

--- examples/full/pages/starship/+Layout.vue
+++ examples/full/pages/starship/+Layout.vue
@@ -15,7 +15,8 @@
   </p>
   <br />
   <div class="sub-navigation">
-    <a keep-scroll-position href="/starship">Overview</a> <a keep-scroll-position href="/starship/reviews">Reviews</a>
+    <a keep-scroll-position href="/starship">Overview</a>
+    <a keep-scroll-position href="/starship/reviews">Reviews</a>
     <a keep-scroll-position href="/starship/spec">Tech Spec</a>
   </div>
   <div style="margin-top: 20px; border: 1px solid black; padding: 10px 40px">
