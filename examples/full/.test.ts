9dd1e64fee94dfbafa079b925e19f56ea40e6587
Romuald Brillout <git@brillout.com>
2024-07-19 14:34:56 +0200

    Revert "add test"
    This reverts commit f70413528032ca4c8510cf4b114c525709bc2e12.

--- examples/full/.test.ts
+++ examples/full/.test.ts
@@ -165,11 +165,6 @@ function findFirstPageId(html: string) {
   return pageId
 }
 
-function expectBodyHtmlEnd(html: string) {
-  expect(html).toContain('<div id="bodyHtmlEnd-test"></div>')
-  expect(html).toContain('<div id="teleported"></div>')
-}
-
 async function expectHelloPage(name: 'anonymous' | 'jon' | 'alice' | 'evan' | 'eli') {
   await autoRetry(async () => {
     expect(await page.textContent('h1')).toContain('Hello')
