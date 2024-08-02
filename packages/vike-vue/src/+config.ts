85d2656253f01fd2f23653f8ef6d9378fb72eeca
Romuald Brillout <git@brillout.com>
2024-08-02 09:21:05 +0200

    fix: make favicon setting global
    BREAKING CHANGE: The setting `favicon` now only accepts one global
    value, see https://vike.dev/favicon#global

--- packages/vike-vue/src/+config.ts
+++ packages/vike-vue/src/+config.ts
@@ -83,7 +83,8 @@ export default {
       env: { server: true },
     },
     favicon: {
-      env: { server: true, client: true },
+      env: { server: true },
+      global: true,
     },
     lang: {
       env: { server: true, client: true },
--- packages/vike-vue/src/renderer/onRenderClient.ts
+++ packages/vike-vue/src/renderer/onRenderClient.ts
@@ -3,7 +3,7 @@ export { onRenderClient }
 
 import { createVueApp, type ChangePage } from './createVueApp.js'
 import { getHeadSetting } from './getHeadSetting.js'
-import type { OnRenderClientAsync, PageContextClient } from 'vike/types'
+import type { OnRenderClientAsync } from 'vike/types'
 import { callCumulativeHooks } from '../utils/callCumulativeHooks.js'
 import type { App } from 'vue'
 import { objectAssign } from '../utils/objectAssign.js'
@@ -31,7 +31,6 @@ const onRenderClient: OnRenderClientAsync = async (pageContext): ReturnType<OnRe
 
     const title = getHeadSetting('title', pageContext) || ''
     const lang = getHeadSetting('lang', pageContext) || 'en'
-    const favicon = getHeadSetting('favicon', pageContext)
 
     if (title !== undefined)
       // We skip if the value is undefined because we shouldn't remove values set in HTML (by the Head setting).
@@ -41,21 +40,5 @@ const onRenderClient: OnRenderClientAsync = async (pageContext): ReturnType<OnRe
       //    can set the value to `null` to ensure that previous values are overridden.
       document.title = title
     if (lang !== undefined) document.documentElement.lang = lang
-    if (favicon !== undefined) setFavicon(favicon)
   }
-}
-
-// https://stackoverflow.com/questions/260857/changing-website-favicon-dynamically/260876#260876
-function setFavicon(faviconUrl: string | null) {
-  let link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']")
-  if (!faviconUrl) {
-    if (link) document.head.removeChild(link)
-    return
-  }
-  if (!link) {
-    link = document.createElement('link')
-    link.rel = 'icon'
-    document.head.appendChild(link)
-  }
-  link.href = faviconUrl
 }
