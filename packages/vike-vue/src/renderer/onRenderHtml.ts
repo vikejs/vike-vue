1dc1f48dd36c36799b4618f8c0b193e4b41a66a7
Romuald Brillout <git@brillout.com>
2024-08-08 11:41:28 +0200

    fix: don't unnecessarily pass useConfig() values to client-side

--- packages/vike-vue/src/renderer/onRenderHtml.ts
+++ packages/vike-vue/src/renderer/onRenderHtml.ts
@@ -93,6 +93,9 @@ async function getHeadHtml(pageContext: PageContextServer & PageContextInternal)
   const { app } = await createVueApp(pageContext, true, 'Head')
   headElementHtml = dangerouslySkipEscape(await renderToStringWithErrorHandling(app))
 
+  // Not needed on the client-side, thus we remove it to save KBs sent to the client
+  delete pageContext._configFromHook
+
   const headHtml = escapeInject`
     ${titleTags}
     ${viewportTag}
