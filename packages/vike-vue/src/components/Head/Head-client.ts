e5805462e4827a725469bc5b72dd1543fa592c7f
Muhammad <phonzammizaki@gmail.com>
2024-08-09 00:39:43 +0700

    feat: new component `<Head>`

---
+++ packages/vike-vue/src/components/Head/Head-client.ts
@@ -0,0 +1,25 @@
+export { Head }
+
+// Same as ./Head-server.ts but importing useConfig-client.js
+import { useConfig } from '../../hooks/useConfig/useConfig-client.js'
+import { defineComponent } from 'vue'
+
+/**
+ * Add arbitrary `<head>` tags.
+ *
+ * (The children are teleported to `<head>`.)
+ *
+ * https://vike.dev/Head
+ */
+const Head = defineComponent({
+  name: 'Head',
+  inheritAttrs: false,
+  setup:
+    (_props, { slots }) =>
+    () => {
+      if (!slots.default) return
+      const config = useConfig()
+      config({ Head: slots.default() })
+      return null
+    },
+})
