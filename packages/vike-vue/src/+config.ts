ba3378c5e41bd489e26345f8c98a92a7df39cd66
Romuald Brillout <git@brillout.com>
2024-09-12 12:40:58 +0200

    minor refactor

--- packages/vike-vue/src/+config.ts
+++ packages/vike-vue/src/+config.ts
@@ -1,8 +1,10 @@
+export { config as default }
+
 import type { Config } from 'vike/types'
 import './utils/tsx-workaround.js'
 import { ssrEffect } from './integration/ssrEffect.js'
 
-export default {
+const config = {
   name: 'vike-vue',
   require: {
     vike: '>=0.4.191',
