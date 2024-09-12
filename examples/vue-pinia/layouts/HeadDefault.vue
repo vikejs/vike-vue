b80098a4010c12b4c62a4a3c23940af3ebdcd474
Romuald Brillout <git@brillout.com>
2024-09-12 13:40:09 +0200

    simplify example

--- examples/vue-pinia/layouts/HeadDefault.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <meta name="description" content="Demo showcasing Vike + Vue + Pinia" />
-  <link rel="icon" :href="logoUrl" />
-</template>
-
-<script lang="ts" setup>
-import logoUrl from '../assets/logo.svg'
-</script>
--- examples/vue-pinia/vite.config.ts
+++ examples/vue-pinia/vite.config.ts
@@ -1,14 +1,9 @@
+export { config as default }
+
 import vue from '@vitejs/plugin-vue'
 import vike from 'vike/plugin'
 import { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [
-    vike({ prerender: true }),
-    vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-  ],
+  plugins: [vike(), vue()],
 }
-
-export default config
