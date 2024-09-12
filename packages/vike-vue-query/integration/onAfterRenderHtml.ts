892bf802bab299413aa2167d2c9ef06780da521c
Romuald Brillout <git@brillout.com>
2024-09-12 17:25:17 +0200

    minor refactor

--- packages/vike-vue-query/integration/onAfterRenderHtml.ts
+++ packages/vike-vue-query/integration/onAfterRenderHtml.ts
@@ -4,5 +4,10 @@ import { dehydrate } from '@tanstack/vue-query'
 import type { OnAfterRenderHtmlSync } from 'vike-vue/types'
 
 const onAfterRenderHtml: OnAfterRenderHtmlSync = (pageContext): ReturnType<OnAfterRenderHtmlSync> => {
+  dehydrateVueQuery(pageContext)
+}
+
+type PageContext = Parameters<typeof onAfterRenderHtml>[0]
+function dehydrateVueQuery(pageContext: PageContext) {
   pageContext._vueQueryInitialState = dehydrate(pageContext.queryClient!)
 }
