1b835e47762b2f44a725ef37625258e656c09f04
Romuald Brillout <git@brillout.com>
2025-04-07 23:04:56 +0200

    docs: vike-vue-query

--- examples/vue-query/pages/+queryClientConfig.ts
+++ examples/vue-query/pages/+queryClientConfig.ts
@@ -2,10 +2,11 @@ export { queryClientConfig }
 
 import type { QueryClientConfig } from '@tanstack/vue-query'
 
-// set query client options - e.g. not refetching when window loses or gains focus during development
+// Query client options - e.g.
 const queryClientConfig: QueryClientConfig = {
   defaultOptions: {
     queries: {
+      // Don't refetch when window loses or gains focus during development
       refetchOnWindowFocus: import.meta.env.PROD,
       // staleTime: 1000 * 60 * 5,
     },
