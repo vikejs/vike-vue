e4fa883046e2ab6996e1730519953d86a22f02ea
Romuald Brillout <git@brillout.com>
2025-05-25 16:04:09 +0200

    minor refactor

--- examples/vue-pinia/pages/index/+data.ts
+++ examples/vue-pinia/pages/index/+data.ts
@@ -1,13 +1,19 @@
-import { PageContext } from 'vike/types'
-
+// Environment: server
+export { data }
 export type Data = Awaited<ReturnType<typeof data>>
 
-export const data = async (pageContext: PageContext) => {
+import type { PageContextServer } from 'vike/types'
+
+async function data(pageContext: PageContextServer) {
   const todosInit = await fetchTodosInit()
   return { todosInit }
 }
 
 // Pretending the list is fetched over the network
-const fetchTodosInit = async () => {
-  return [{ text: 'Buy apples' }, { text: `Update Node.js ${process.version} to latest version` }]
+async function fetchTodosInit() {
+  return [
+    //
+    { text: 'Buy apples' },
+    { text: `Update Node.js ${process.version} to latest version` },
+  ]
 }
