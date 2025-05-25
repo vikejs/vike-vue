edce0f2c97775dce9219be304c9a7116e93a5616
Romuald Brillout <git@brillout.com>
2025-05-25 16:00:26 +0200

    minor refactor

--- examples/vue-pinia/pages/index/+onData.ts
+++ examples/vue-pinia/pages/index/+onData.ts
@@ -1,9 +1,13 @@
-import { PageContext } from 'vike/types'
-import { useTodoStore } from '../../stores/useTodoStore'
-import type { Data } from './+data'
+// Environment: server, client
+export { onData }
 
-export const onData = (pageContext: PageContext & { data?: Data }) => {
-  useTodoStore(pageContext.pinia!).initTodos(pageContext.data!.todosInit)
+import type { PageContext } from 'vike/types'
+import type { Data } from './+data'
+import { useTodoStore } from '../../stores/useTodoStore'
+
+function onData(pageContext: PageContext & { data?: Data }) {
+  const { initTodos } = useTodoStore(pageContext.pinia!)
+  initTodos(pageContext.data!.todosInit)
 
   // Saving KBs: we don't need pageContext.data (we use the store instead)
   // - If we don't delete pageContext.data then Vike sends pageContext.data to the client-side
