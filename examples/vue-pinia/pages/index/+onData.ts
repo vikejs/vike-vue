0ab71809e0dc0a4912ba5cc81228846d14d3dc7b
Romuald Brillout <git@brillout.com>
2025-05-25 15:56:36 +0200

    comment

--- examples/vue-pinia/pages/index/+onData.ts
+++ examples/vue-pinia/pages/index/+onData.ts
@@ -4,5 +4,9 @@ import type { Data } from './+data'
 
 export const onData = (ctx: PageContext & { data?: Data }) => {
   useTodoStore(ctx.globalContext?.pinia || ctx.pinia!).initTodos(ctx.data?.todosInit ?? [])
+
+  // Saving KBs: we don't need pageContext.data (we use the store instead)
+  // - If we don't delete pageContext.data then Vike sends pageContext.data to the client-side
+  // - This optimization only works if the page is SSR'd: if the page is pre-rendered then don't do this
   if (!ctx.isPrerendering) delete ctx.data
 }
