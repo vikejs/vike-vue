9d984ecf583d706b59a00ed47e1c8c55f6b9e407
Rom <git@brillout.com>
2025-12-06 18:45:26 +0100

    fix: support client persistence (fix vikejs/vike#2881) (#220)

--- packages/vike-vue-pinia/integration/onCreateApp.ts
+++ packages/vike-vue-pinia/integration/onCreateApp.ts
@@ -10,7 +10,14 @@ function onCreateApp(pageContext: PageContext) {
   if (pageContext.isClientSide) {
     const pinia = createPinia()
     const { _piniaInitialState } = pageContext
-    if (_piniaInitialState) pinia.state.value = _piniaInitialState
+    if (_piniaInitialState) {
+      pinia.state.value = {
+        ..._piniaInitialState,
+        // Preserve client-side persisted state — e.g. `localStorage`
+        // https://github.com/vikejs/vike/issues/2881
+        ...pinia.state.value,
+      }
+    }
     pageContext.globalContext.pinia = pinia
   }
 
