369f37a8ec91016680b66271d25bad55d03cc723
Romuald Brillout <git@brillout.com>
2025-12-08 14:02:35 +0100

    update to-do

---
+++ examples/vue-pinia/pages/+onCreatePinia.ts
@@ -0,0 +1,3 @@
+export function nonCreatePinia() {
+  // TODO/copilot implement, see https://prazdevs.github.io/pinia-plugin-persistedstate/guide/ and https://github.com/vikejs/vike/issues/2881#issuecomment-3624267205 and also modify the example to use a persisted store
+}
