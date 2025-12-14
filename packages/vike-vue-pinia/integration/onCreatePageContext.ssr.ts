21826d93f41519e7a727be83337a8069c0d57b94
Romuald Brillout <git@brillout.com>
2025-12-14 23:08:54 +0100

    minor refactor

--- packages/vike-vue-pinia/integration/onCreatePageContext.ssr.ts
+++ packages/vike-vue-pinia/integration/onCreatePageContext.ssr.ts
@@ -4,6 +4,5 @@ import type { PageContextServer } from 'vike/types'
 import { createPinia } from 'pinia'
 
 function onCreatePageContext(pageContext: PageContextServer) {
-  if (pageContext.isClientSideNavigation) return // only SSR
   pageContext.pinia = createPinia()
 }
