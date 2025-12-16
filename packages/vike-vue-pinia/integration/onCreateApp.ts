7f5205cfa16fbfd8c0fb8f797b187378a45a2308
Romuald Brillout <git@brillout.com>
2025-12-16 22:52:07 +0100

    fix: revert fake client persistence support (#220) (vikejs/vike#2881)

--- packages/vike-vue-pinia/integration/onCreateApp.ts
+++ packages/vike-vue-pinia/integration/onCreateApp.ts
@@ -10,14 +10,7 @@ async function onCreateApp(pageContext: PageContext) {
   if (pageContext.isClientSide) {
     const pinia = await createPiniaPlus(pageContext, true)
     const { _piniaInitialState } = pageContext
-    if (_piniaInitialState) {
-      pinia.state.value = {
-        ..._piniaInitialState,
-        // Preserve client-side persisted state — e.g. `localStorage`
-        // https://github.com/vikejs/vike/issues/2881
-        ...pinia.state.value,
-      }
-    }
+    if (_piniaInitialState) pinia.state.value = _piniaInitialState
   }
 
   app.use(pageContext.globalContext.pinia ?? pageContext.pinia!)
