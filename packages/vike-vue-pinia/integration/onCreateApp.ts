c5cbec9cf33ce85ea5a7f840cad35cd356b54f83
Romuald Brillout <git@brillout.com>
2025-12-16 21:31:13 +0100

    debug

--- packages/vike-vue-pinia/integration/onCreateApp.ts
+++ packages/vike-vue-pinia/integration/onCreateApp.ts
@@ -9,10 +9,17 @@ async function onCreateApp(pageContext: PageContext) {
 
   if (pageContext.isClientSide) {
     const pinia = await createPiniaPlus(pageContext, true)
+    console.log('pinia.state.value', pinia.state.value)
+    console.log('pinia.state.value.persistedCounter', pinia.state.value.persistedCounter)
     pageContext._piniaInitialStateClient = pinia.state.value
     const { _piniaInitialState } = pageContext
+    // @ts-ignore
+    console.log('_piniaInitialState.counter.count', _piniaInitialState.counter.count)
+    // @ts-ignore
+    console.log('_piniaInitialState.persistedCounter.count', _piniaInitialState.persistedCounter.count)
+    console.log('_piniaInitialState', _piniaInitialState)
     if (_piniaInitialState) {
-      pinia.state.value = _piniaInitialState
+      pinia.state.value = { ..._piniaInitialState }
     }
   }
 
