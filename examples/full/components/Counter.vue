5c4aacce3990c740a62f9ec96d044612ec5ca711
Romuald Brillout <git@brillout.com>
2025-12-21 23:49:46 +0100

    update to-do

--- examples/full/components/Counter.vue
+++ examples/full/components/Counter.vue
@@ -4,6 +4,9 @@
 
 <script lang="ts">
 import { reactive } from 'vue'
+// TODO/ai
+// 1. add useHydrated example
+// 2. update jsdocs https://github.com/vikejs/vike-vue/blob/68c513d15ff2f57081a7d803e4e861c2edcc818b/packages/vike-vue/src/hooks/useHydrated.ts#L15-L22
 export default {
   setup(props) {
     const state = reactive({ count: 0 })
